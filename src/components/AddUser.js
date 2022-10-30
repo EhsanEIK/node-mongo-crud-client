d024002234e5f1884c2d45bf02cf806a836b6255
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-30 09:02:33 +0600

    initial commit

---
+++ src/components/AddUser.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AddUser = () => {
+    return (
+        <div>
+            <h2>Please add a new user</h2>
+        </div>
+    );
+};
+
+export default AddUser;
