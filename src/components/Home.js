535aba2744d089a33f6ae6bb8752c98226ba2808
ehsankhan1997.ek@gmail.com <ehsankhan1997.ek@gmail.com>
2022-10-30 11:46:56 +0600

    added handlerDelete function to delete single user

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,14 +1,35 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
 
 const Home = () => {
     const users = useLoaderData();
+    const [displayUsers, setDisplayUsers] = useState(users);
+
+    const handleDelete = user => {
+        const { _id, name } = user;
+        const agree = window.confirm(`Are you confirm to delete ${name}?`);
+        if (agree) {
+            fetch(`http://localhost:5000/users/${_id}`, {
+                method: "DELETE"
+            })
+                .then(res => res.json())
+                .then(data => {
+                    if (data.deletedCount > 0) {
+                        alert("User Deleted Successfully");
+                        const remainingUsers = displayUsers.filter(usr => usr._id !== _id);
+                        setDisplayUsers(remainingUsers);
+                    }
+                })
+        }
+    }
 
     return (
         <div>
-            <h2>All user: {users.length}</h2>
+            <h2>All user: {displayUsers.length}</h2>
             {
-                users.map(user => <p key={user._id}>{user.email}</p>)
+                displayUsers.map(user => <p key={user._id}>
+                    {user.email} <button onClick={() => handleDelete(user)}>X</button>
+                </p>)
             }
         </div>
     );
